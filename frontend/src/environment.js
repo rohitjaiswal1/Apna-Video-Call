b0e466f7af2d7afdd34b5e8b9a7752e73813cc6b
Rohit Jaiswal <112792674+rohitjaiswal1@users.noreply.github.com>
2025-02-05 15:03:52 +0530

    Update environment.js

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,6 +1,6 @@
 let IS_PROD = false;
 const server = IS_PROD
-  ? "https://apnacollegebackend.onrender.com"
+  ? "https://apnavideobackend-rnu3.onrender.com"
   : "http://localhost:8000";
 
 export default server;
